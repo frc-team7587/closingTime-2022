a16df9f3b6062b4cea51442330075f7494d13f34
Alan Tang <alantango@gmail.com>
2022-02-13 20:37:07 -0500

    add missed updates on Arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -9,15 +9,13 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
-
     private final CANSparkMax m_spark = new CANSparkMax(1, MotorType.kBrushless);
-
     public void raise() {
-        m_spark.set(0.25 * -ARM_MAX_SPEED);
+        m_spark.set(-ARM_MAX_SPEED);
     }
 
     public void lower() {
-        m_spark.set(0.25 * ARM_MAX_SPEED);
+        m_spark.set(ARM_MAX_SPEED);
     }
 
     public void stop() {
@@ -27,7 +25,6 @@ public class Arm extends SubsystemBase {
     public void setSoftLimits(){
         m_spark.enableSoftLimit(SoftLimitDirection.kForward, true);
         m_spark.enableSoftLimit(SoftLimitDirection.kReverse, true);
-
         m_spark.setSoftLimit(SoftLimitDirection.kForward, ARM_SOFT_LIMIT_FWD);
         m_spark.setSoftLimit(SoftLimitDirection.kReverse, ARM_SOFT_LIMIT_BKW);
     }
@@ -40,5 +37,4 @@ public class Arm extends SubsystemBase {
     public void periodic() {
         // This method will be called once per scheduler run
     }
-
 }
