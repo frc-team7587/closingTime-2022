65317735d7f188d5e2c3aef8c90076bb56c4d31d
Alan Tang <alantango@gmail.com>
2022-02-19 22:30:43 -0500

    add experiment code for PID controlled movement

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,9 @@ public class RobotContainer {
     private final DriveTrain m_drive = new DriveTrain();
     private final Arm m_arm = new Arm();
     private final Intake m_intake = new Intake();
+
+    // experiment: PID controlled Arm system
+    private final ArmSubsystem m_robotArm = new ArmSubsystem();
     
     final XboxController xbox = new XboxController(XBOX_CTRL_PORT);
     final Joystick logi = new Joystick(LOGIJOY_PORT);
@@ -38,10 +41,10 @@ public class RobotContainer {
     private void configureButtonBindings() {
 
         // Intake 
-        final JoystickButton btnIn = new JoystickButton(xbox, Button.kLeftBumper.value);
+        final JoystickButton btnIn = new JoystickButton(xbox, Button.kLeftBumper.value);  // bumper left
             btnIn.whenPressed( new IntakeIn(m_intake, () -> !btnIn.get()) );
 
-        final JoystickButton btnOut = new JoystickButton(xbox, Button.kRightBumper.value);
+        final JoystickButton btnOut = new JoystickButton(xbox, Button.kRightBumper.value);  // bumper right
             btnIn.whenPressed( new IntakeIn(m_intake, () -> !btnOut.get()) );
 
         // Arm
@@ -51,7 +54,40 @@ public class RobotContainer {
        final JoystickButton btnArmUp = new JoystickButton(xbox, Button.kB.value);   // B button
            btnArmUp.whenPressed( new ArmUp(m_arm, () -> !btnArmUp.get()) );
 
+        configurePIDControlledArmSystem();
+
     }
+
+    private void configurePIDControlledArmSystem(){
+        new JoystickButton(xbox, Button.kA.value)      // A button
+        .whenPressed(
+            () -> {
+              m_robotArm.setGoal(2);
+              m_robotArm.enable();
+            },
+            m_robotArm);
+
+    // Move the arm to neutral position when the 'X' button is pressed.
+    new JoystickButton(xbox, Button.kX.value)       // X button
+        .whenPressed(
+            () -> {
+              m_robotArm.setGoal(Constants.ArmConstants.kArmOffsetRads);
+              m_robotArm.enable();
+            },
+            m_robotArm);
+
+    // Disable the arm controller when Back is pressed
+    new JoystickButton(xbox, Button.kBack.value)
+            .whenPressed(m_robotArm::disable);
+    }
+
+   /**
+   * Disables all ProfiledPIDSubsystem and PIDSubsystem instances. This should be called on robot
+   * disable to prevent integral windup.
+   */
+  public void disablePIDSubsystems() {
+    m_robotArm.disable();
+  }
 
     public Command getAutonomousCommand() {
         return null;
